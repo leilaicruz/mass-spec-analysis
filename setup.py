b1d40e0688b509319f60145c68af2078bae4cff8
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2021-08-04 12:01:18 +0200

    initialize git

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyis of mass spec data to know the function of nrp1 in budding yeast',
+    author='Laan Lab',
+    license='MIT',
+)
